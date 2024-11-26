179a31514585dd30b75b9e690cfab024c9bda4ea
Asier Bujedo Álvarez <asier.bujedo@opendeusto.es>
2024-11-26 16:53:31 +0000

    Node behaviours
    Co-Authored-By: Lander Gallastegi <LNDF@users.noreply.github.com>

--- ros_workspace/src/proyecto/src/control_robot.py
+++ ros_workspace/src/proyecto/src/control_robot.py
@@ -2,6 +2,7 @@
 
 import sys
 import rospy
+from commands import Command
 from moveit_commander import MoveGroupCommander, RobotCommander, roscpp_initialize, PlanningSceneInterface
 from std_msgs.msg import Int32
 from geometry_msgs.msg import Pose, PoseStamped, Point, Quaternion
@@ -28,22 +29,22 @@ class ControlRobot:
     def handle_command(self, msg: Int32) -> None:
         # Acción a realizar en función de la orden recibida
         command = msg.data
-        if command == 1:
+        if command == Command.COGER_FRUTA:
             rospy.loginfo("Coger fruta")
             self.move_to_specific_position("coger_fruta")
-        elif command == 2:
+        elif command == Command.POSICION_INICIAL:
             rospy.loginfo("Posición inicial")
             self.move_to_specific_position("posicion_inicial")
-        elif command == 3:
+        elif command == Command.CAJA_BUENA_ARRIBA:
             rospy.loginfo("Caja 1 arriba")
             self.move_to_specific_position("caja_1_arriba")
-        elif command == 4:
+        elif command == Command.CAJA_BUENA_ABAJO:
             rospy.loginfo("Caja 1 abajo")
             self.move_to_specific_position("caja_1_abajo")
-        elif command == 5:
+        elif command == Command.CAJA_MALA_ARRIBA:
             rospy.loginfo("Caja 2 arriba")
             self.move_to_specific_position("caja_2_arriba")
-        elif command == 6:
+        elif command == Command.CAJA_MALA_ABAJO:
             rospy.loginfo("Caja 2 abajo")
             self.move_to_specific_position("caja_2_abajo")
         else:
@@ -117,28 +118,6 @@ class ControlRobot:
             rospy.logwarn("No se pudo ejecutar la trayectoria completa")
 
     def move_to_specific_position(self, action: str) -> None:
-        # Define las poses y configuraciones de articulaciones para cada acción
-
-        poses = {
-            "coger_fruta": {
-                "angles": [0.6133421063423157, -0.7791211170009156, 0.6027692000018519, -1.4883136761239548, -1.5353906790362757, 0.5786222219467163]
-            },
-            "posicion_inicial": {
-                "angles": [1.0743645429611206, -0.9983374041369935, 0.4319809118853968, -1.0997422498515625, -1.5645702520953577, -0.11793691316713506]
-            },
-            "caja_1_arriba":{
-                "angles": [1.7889834642410278, -1.310633049612381, 0.4467585722552698, -0.8000471752933045, -1.614591423665182, -1.3403261343585413]
-            },
-            "caja_1_abajo": {
-                "angles": [1.788036584854126, -1.6146403751769007, 1.7082722822772425, -1.7573992214598597, -1.6137712637530726, -1.3365376631366175]
-            },
-            "caja_2_arriba":{
-                "angles": [-0.2870891729937952, -1.487642579977848, 0.627301041279928, -0.7119994324496766, -1.5659797827350062, -1.353751007710592]
-            },
-            "caja_2_abajo": {
-                "angles": [-0.28807336488832647, -1.7917567692198695, 1.7556470076190394, -1.536181890671589, -1.564662281666891, -1.3506696859942835]
-            }
-        }
 
         joints = {
             "coger_fruta": [0.6133421063423157, -0.7791211170009156, 0.6027692000018519, -1.4883136761239548, -1.5353906790362757, 0.5786222219467163],
@@ -149,32 +128,8 @@ class ControlRobot:
             "caja_2_abajo": [-0.28807336488832647, -1.7917567692198695, 1.7556470076190394, -1.536181890671589, -1.564662281666891, -1.3506696859942835]
         }
 
-        for act_joint in joints:
-            if action == act_joint:
-                self.move_to_configuration(joints[act_joint])
-                break
-
-
-        # if action in poses and action in joints:
-        #     self.move_group.set_pose_target(poses[action])
-        #     if self.move_group.go(wait=True):
-        #         rospy.loginfo(f"Robot movido a la posición {action}")
-        #     else:
-        #         rospy.logwarn("No se pudo mover a la posición deseada")
-
-        #     self.move_group.stop()
-        #     self.move_group.clear_pose_targets()
-
-        #     # Mover a la configuración de juntas
-        #     joint_goal = joints[action]
-        #     if self.move_group.go(joint_goal, wait=True):
-        #         rospy.loginfo(f"Robot movido a la configuración de juntas para {action}")
-        #     else:
-        #         rospy.logwarn("No se pudo mover a la configuración de juntas deseada")
-
-        #     self.move_group.stop()
-        # else:
-        #     rospy.logwarn(f"Acción no reconocida: {action}")
+        if action in joints:
+            self.move_to_configuration(joints[action])
 
 if __name__ == '__main__':
     try:
